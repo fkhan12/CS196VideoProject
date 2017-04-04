0992b2d167536458622f9759fbfb55f2e4e8e60f
Madison Minnick <maddieminnick@wirelessprv-10-192-159-195.near.illinois.edu>
2017-04-03 23:25:55 -0500

    test

--- __init__.py
+++ __init__.py
@@ -3,9 +3,10 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template(homepage.html)
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name = None):
+    return render_template(homepage.html, name = name)
 
 
 if __name__=="__main__":
